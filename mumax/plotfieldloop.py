a25790d4e03a8a30b1334bf4abda5baac0f16669
Sam Liu <samktliu@gmail.com>
2025-11-05 10:50:50 -0700

    mum

--- mumax/plotfieldloop.py
+++ mumax/plotfieldloop.py
@@ -14,14 +14,14 @@ for f in folders:
         data = np.genfromtxt(fname)
         mz = data[:,3]
         mzs.append(mz)
-        Bz = data[:,-1]
+        Bz = data[:,-1]*1e4
     mzs_arr = np.array(mzs).T
     p = []
     for i in range(len(Bz)):
         p.append(np.sum(mzs_arr[i,:]>0)/len(mzs))
     p = np.array(p)
-    np.save(f'p_{f[2:-1]}',p)
-    plt.plot(Bz,p,'^-')
-np.save(f'Bz.npy',Bz)
+    p = np.concatenate([Bz,p]).reshape(2,len(Bz)).T
+    np.save(f'pmm_{f[2:-1]}',p)
+    plt.plot(p[:,0],p[:,1],'^-')
 
 plt.savefig('test.svg')
